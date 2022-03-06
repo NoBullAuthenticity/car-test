18439f34e891ec3b328579b8d71c7fe3fa8eb0c3
NoBullAuthenticity <nobullsecureyourauthenticity@gmail.com>
2022-03-06 09:15:01 -0500

    third day

---
+++ toggled-search-bar.js
@@ -0,0 +1,8 @@
+function shownavsearchbar() {
+    var x = document.getElementById("navsearch");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
